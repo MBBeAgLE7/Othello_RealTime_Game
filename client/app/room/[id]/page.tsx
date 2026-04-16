c7338f83016e2cf661b8ae8dbe555f08c637135f
MBBeAgLE7 <120193425+MBBeAgLE7@users.noreply.github.com>
2026-04-16 21:02:41 +0530

    6

--- client/app/room/[id]/page.tsx
+++ client/app/room/[id]/page.tsx
@@ -1,14 +1,148 @@
-'use client'
+"use client";
+import { useParams, useRouter } from 'next/navigation'
+import { useCallback, useEffect, useState } from "react";
+import { toast } from "react-toastify";
+import { useMutation, useQuery, useSubscription } from "@apollo/client";
+import GameScreen from "@/_components/GameScreen";
+import { ADD_PLAYER } from "@/_graphql/mutations";
+import { GET_GAME_BY_ID } from "@/_graphql/queries";
+import graphQLError from "@/_utilities/graphQLError";
+import { GAME_UPDATED, GAMER_CONNECTION } from "@/_graphql/subscriptions";
+import { useGame } from "@/_providers/GameProvider";
+import useLoader from "@/_store/useLoader";
+import { MAX_GAMER_COUNT } from "@/_constants";
+import { EGamerStatus, ELocalStorage } from "@/_enums";
+import { IGame, IGamerConnection, SubscriptionGamerConnection, SubscriptionGameUpdatedData } from "@/_types";
 
-import { useParams } from 'next/navigation'
 
-export default function RoomPage() {
-  const params = useParams()
-  const roomId = params.id
+function toggleLocalStorageGameStartedMessage(status: boolean) {
+    if (status) {
+        localStorage.setItem(ELocalStorage.GAME_STARTED_MESSAGE_SHOWN, "true");
+    } else {
+        localStorage.removeItem(ELocalStorage.GAME_STARTED_MESSAGE_SHOWN);
+    }
+}
 
-  return (
-    <div>
-      <h1>Room ID: {roomId}</h1>
-    </div>
-  )
+
+export default function Page() {
+    const { id } = useParams();
+    const router = useRouter()
+    const { setGameID } = useGame();
+    const [ game, setGame ] = useState<IGame>();
+    const { onOpen, onClose } = useLoader();
+    const userID: string = localStorage.getItem(ELocalStorage.USERID)!
+
+    const [ addPlayer ] = useMutation(ADD_PLAYER, {
+        onError: graphQLError
+    });
+    useQuery<{ game: IGame }>(GET_GAME_BY_ID, {
+        variables: {
+            id,
+        },
+        onCompleted: ({ game }) => {
+            if (!game) return;
+
+            onClose();
+            setGame(game);
+            const isAuthenticatedGamer: boolean = game.gamers.some(gamer => gamer.id == userID);
+
+            if (game.gamers.length == MAX_GAMER_COUNT && !isAuthenticatedGamer) {
+                return router.push("/");
+            }
+
+            if (game.isGameFinished) {
+                toggleLocalStorageGameStartedMessage(false);
+                return showGameFinishedMessage();
+            }
+
+            if (game.isGameStarted) return;
+
+            addPlayer({
+                variables: {
+                    data: {
+                        gameID: id,
+                        gamerID: userID
+                    }
+                }
+            })
+        },
+        onError: (error) => {
+            onClose();
+            graphQLError(error)
+        },
+    });
+
+    const showGameFinishedMessage = useCallback(() => {
+        const message: string = "Game has finished!"
+        toast.info(message, {
+            toastId: message
+        })
+    }, [])
+
+    useSubscription<SubscriptionGameUpdatedData>(GAME_UPDATED, {
+        variables: {
+            gameID: id,
+        },
+        onData: ({ data: { data } }) => {
+            if (!data?.game) return;
+
+            if (data.game.isGameStarted && !localStorage.getItem(ELocalStorage.GAME_STARTED_MESSAGE_SHOWN)) {
+                toggleLocalStorageGameStartedMessage(true);
+                const message: string = "Game has started. You can start playing now."
+                toast.info(message, {
+                    toastId: message
+                })
+            }
+
+            if (data.game.isGameFinished) {
+                toggleLocalStorageGameStartedMessage(false);
+                if (data.game.exitGamer && data.game.exitGamer != userID) {
+                    const message: string = "Your opponent has exited.Game has finished!"
+                    toast.info(message, {
+                        toastId: message
+                    })
+                } else if (!data.game.exitGamer) {
+                    showGameFinishedMessage();
+                }
+            }
+        },
+        onError: graphQLError
+    })
+
+    useSubscription<SubscriptionGamerConnection>(GAMER_CONNECTION, {
+        variables: { gameID: id },
+        onData: ({ data: { data } }) => {
+
+            if (!data?.gamer || !game) return;
+            const gamer = data?.gamer as IGamerConnection;
+
+            if (gamer.userID == userID) return;
+
+            const color = (game as IGame).gamers.find(gamerItem => gamerItem.id == gamer.userID)?.color;
+            if (!color) return;
+
+            const message: string = `${color} gamer ${gamer.status.toLowerCase()}!`
+
+            if (gamer.status == EGamerStatus.CONNECTED) {
+                toast.success(message, {
+                    toastId: message
+                })
+            } else {
+                toast.error(message, {
+                    toastId: message
+                })
+            }
+        },
+        onError: graphQLError
+    });
+
+    useEffect(() => {
+        setGameID(id as string);
+        onOpen();
+        return () => {
+            onClose();
+        }
+    }, [])
+
+    return <GameScreen/>;
 }
