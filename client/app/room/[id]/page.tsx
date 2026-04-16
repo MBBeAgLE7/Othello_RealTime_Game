d97790682371ce3de8c95743aa8b145235f5c23e
MBBeAgLE7 <120193425+MBBeAgLE7@users.noreply.github.com>
2026-04-16 20:43:30 +0530

    5

---
+++ client/app/room/[id]/page.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import { useParams } from 'next/navigation'
+
+export default function RoomPage() {
+  const params = useParams()
+  const roomId = params.id
+
+  return (
+    <div>
+      <h1>Room ID: {roomId}</h1>
+    </div>
+  )
+}
